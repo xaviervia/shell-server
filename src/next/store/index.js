fde9d7072314f393f598538a50b48a5bbcc540e0
Xavier Via <fernando.via@gmail.com>
2016-06-09 00:24:00 +0200

    Add Remote Redux DevTools

--- src/next/store/index.js
+++ src/next/store/index.js
@@ -1,9 +1,13 @@
 import { applyMiddleware, createStore, compose } from 'redux'
 import log from './middlewares/log'
 import reducer from './reducer'
+import devTools from 'remote-redux-devtools'
 
-export default () =>
-  compose
-    (applyMiddleware(log))
-    (createStore)
-    (reducer)
+export default () => {
+  const enhancer = compose(
+    applyMiddleware(log),
+    devTools({ realtime: true })
+  )
+
+  return createStore(reducer, {}, enhancer)
+}
